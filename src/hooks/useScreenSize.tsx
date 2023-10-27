1637922e224a61dd40a0e61e524a02303dd6b347
ArturasTv <arturastvaronas@inbox.lt>
2023-10-28 01:29:44 +0300

    BAK-19: create useMediaQuery and useScreenSize hooks

---
+++ src/hooks/useScreenSize.tsx
@@ -0,0 +1,20 @@
+import SCREEN_SIZES from '@/constants/screenSizes';
+import { useMediaQuery } from './useMediaQuery';
+
+function getMaxWidth(screenSize: number) {
+  return `(max-width: ${screenSize}px)`;
+}
+
+function useScreenSize() {
+  const isMobile = useMediaQuery(getMaxWidth(SCREEN_SIZES.mobile));
+  const isTablet = useMediaQuery(getMaxWidth(SCREEN_SIZES.tablet));
+  const isDesktop = useMediaQuery(getMaxWidth(SCREEN_SIZES.desktop));
+
+  return {
+    isMobile,
+    isTablet,
+    isDesktop,
+  };
+}
+
+export { useScreenSize };
